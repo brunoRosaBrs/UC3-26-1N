877dca08c9492e1ed6fe26b72601cbf1202fffbd
Bruno <brunorosabrs1234@gmail.com>
2026-06-10 21:26:37 -0300

    Add files via upload

---
+++ exercicios_aula3/exercicio_escola.sql
@@ -0,0 +1,32 @@
+CREATE TABLE turmas (
+	id SERIAL PRIMARY KEY,
+	nome VARCHAR (10) NOT NULL
+);
+
+CREATE TABLE alunos (
+	id SERIAL PRIMARY KEY, 
+	nome VARCHAR (50) NOT NULL, 
+	email VARCHAR (50) NOT NULL UNIQUE,
+	id_turma INT NOT NULL,
+	FOREIGN KEY (id_turma)
+	REFERENCES turmas (id)
+);
+
+CREATE TABLE professor (
+	id SERIAL PRIMARY KEY,
+	nome VARCHAR (50) NOT NULL,
+	email VARCHAR (50) UNIQUE NOT NULL,
+	especialidade VARCHAR (30) NOT NULL
+);
+
+CREATE TABLE disciplina (
+	id SERIAL PRIMARY KEY,
+	nome VARCHAR (50) NOT NULL,
+	carga_horaria INT NOT NULL
+);
+
+CREATE TABLE curso (
+	id SERIAL PRIMARY KEY,
+	nome VARCHAR (50) NOT NULL,
+	descricao VARCHAR (100) NOT NULL
+);
